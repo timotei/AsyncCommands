83f7f13aeeafe27e1b6a2e6e1b32acd4a2cc6313
timotei <timotei21@gmail.com>
2016-02-07 15:42:49 +0200

    Add PCL project

---
+++ AsyncCommand/AsyncCommandBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+public abstract class AsyncCommandBase : IAsyncCommand
+{
+    public abstract bool CanExecute(object parameter);
+
+    public abstract Task ExecuteAsync(object parameter);
+
+    public async void Execute(object parameter)
+    {
+        await ExecuteAsync(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    protected void RaiseCanExecuteChanged()
+    {
+        var handler = CanExecuteChanged;
+        if (handler != null) handler(this, new PropertyChangedEventArgs("CanExecute"));
+    }
+}
